f298dbcf79e5f3ba17585d3e0f2845b9bf21b038
alexdjetic <alexandredejtic@proton.me>
2024-09-05 17:44:29 +0200

    Feature: argument support secure

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,124 @@
-use std::process;
+// src/main.rs
 mod yaml;
 mod json;
 mod sys_ops;
 mod install_package;
 
-use yaml::{Yaml, open_yaml};
-use json::{Json, open_json};
-use sys_ops::{is_running_as_root, rename_computer};
-use install_package::{install_package_with_apt,
-                      install_package_with_dnf,
-                      install_package_with_homebrew,
-                      install_package_with_pacman,
-                      install_package_with_winget};
+use std::env;
+use std::process;
+use std::path::Path;
+use crate::yaml::{Yaml, open_yaml};
+use crate::json::{Json, open_json};
+use crate::sys_ops::{is_running_as_root, rename_computer};
+use crate::install_package::{install_package_with_apt, install_package_with_dnf, install_package_with_homebrew, install_package_with_pacman, install_package_with_winget};
+
+pub enum FileFormat {
+    Yaml,
+    Json,
+    Unknown,
+}
+
+pub fn determine_file_format(file_path: &str) -> FileFormat {
+    let path = Path::new(file_path);
+    match path.extension().and_then(|ext| ext.to_str()) {
+        Some("yaml") | Some("yml") => FileFormat::Yaml,
+        Some("json") => FileFormat::Json,
+        _ => FileFormat::Unknown,
+    }
+}
+
+fn get_str_value_yaml(yaml: &Yaml, key: &str) -> Option<String> {
+    yaml.get(key).and_then(|v| v.as_str().map(|s| s.to_string()))
+}
+
+fn get_str_value_json(json: &Json, key: &str) -> Option<String> {
+    json.get(key).and_then(|v| v.as_str().map(|s| s.to_string()))
+}
+
+fn extract_packages_yaml(yaml: &Yaml) -> Result<Vec<String>, &'static str> {
+    yaml.get("packages")
+        .and_then(|v| v.as_sequence())
+        .map(|seq| seq.iter().filter_map(|item| item.as_str().map(|s| s.to_string())).collect())
+        .ok_or("Missing or invalid 'packages'")
+}
+
+fn extract_packages_json(json: &Json) -> Result<Vec<String>, &'static str> {
+    json.get("packages")
+        .and_then(|v| v.as_array())
+        .map(|arr| arr.iter().filter_map(|item| item.as_str().map(|s| s.to_string())).collect())
+        .ok_or("Missing or invalid 'packages'")
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Check if the program is running with root or administrative privileges
     if !is_running_as_root() {
         println!("This operation requires root or administrative privileges.");
-        process::exit(1); // Exit with code 1 to indicate lack of privileges
+        process::exit(1);
     }
 
-    // Load the YAML configuration
-    let yaml = open_yaml("example/vm1.yaml")?;
-    let hostname = yaml.get("hostname")
-        .and_then(|v| v.as_str())
-        .ok_or("Missing or invalid 'hostname' in YAML")?;
+    let args: Vec<String> = env::args().collect();
 
-    let package_manager = yaml.get("package_manager")
-        .and_then(|v| v.as_str())
-        .ok_or("Missing or invalid 'package_manager' in YAML")?;
+    if args.len() != 2 {
+        eprintln!("Usage: SetupSmith /path/to/file.yaml");
+        process::exit(1);
+    }
 
-    let packages = yaml.get("packages")
-        .and_then(|v| v.as_sequence())
-        .ok_or("Missing or invalid 'packages' in YAML")?;
-
-    // Rename the computer
-    match rename_computer(hostname) {
-        Ok(()) => {
-            println!("Successfully renamed the computer to '{}'.", hostname);
+    let file_path = &args[1];
+    let file_format = determine_file_format(file_path);
+    let (hostname, package_manager, packages) = match file_format {
+        FileFormat::Yaml => {
+            let yaml = open_yaml(file_path)?;
+            (
+                get_str_value_yaml(&yaml, "hostname").ok_or("Missing or invalid 'hostname'")?,
+                get_str_value_yaml(&yaml, "package_manager").ok_or("Missing or invalid 'package_manager'")?,
+                extract_packages_yaml(&yaml)?,
+            )
+        },
+        FileFormat::Json => {
+            let json = open_json(file_path)?;
+            (
+                get_str_value_json(&json, "hostname").ok_or("Missing or invalid 'hostname'")?,
+                get_str_value_json(&json, "package_manager").ok_or("Missing or invalid 'package_manager'")?,
+                extract_packages_json(&json)?,
+            )
+        },
+        _ => {
+            eprintln!("Unsupported file format.");
+            process::exit(1);
         }
+    };
+
+    match rename_computer(&hostname) {
+        Ok(()) => println!("Successfully renamed the computer to '{}'.", hostname),
         Err(e) => {
             eprintln!("Error renaming computer: {}", e);
-            process::exit(1); // Exit with code 1 to indicate failure
+            process::exit(1);
         }
     }
 
-    // Install packages based on the package manager
     let mut install_results = Vec::new();
-    for package in packages {
-        let package_name = package.as_str().ok_or("Package name is not a string")?;
-        let result = match package_manager {
-            "apt" => install_package_with_apt(package_name),
-            "dnf" => install_package_with_dnf(package_name),
-            "pacman" => install_package_with_pacman(package_name),
-            "homebrew" => install_package_with_homebrew(package_name),
-            "winget" => install_package_with_winget(package_name),
+    for package_name in packages {
+        let result = match package_manager.as_str() {
+            "apt" => install_package_with_apt(&package_name),
+            "dnf" => install_package_with_dnf(&package_name),
+            "pacman" => install_package_with_pacman(&package_name),
+            "homebrew" => install_package_with_homebrew(&package_name),
+            "winget" => install_package_with_winget(&package_name),
             _ => {
                 eprintln!("Unsupported package manager: {}", package_manager);
                 continue;
             }
         };
 
-        // Collect the results of the installation
         match result {
-            Ok(()) => install_results.push(format!("{}", package_name)),
+            Ok(()) => install_results.push(format!("Successfully installed '{}'", package_name)),
             Err(e) => install_results.push(format!("Failed to install '{}': {}", package_name, e)),
         }
     }
 
-    // Print a summary of the operations
     println!("\n--------------------------- Summary ------------------------------------------");
     println!(" > Hostname changed to: '{}'", hostname);
     println!(" > Package Manager used: '{}'", package_manager);
-    println!(" > installed package:");
+    println!(" > Installed packages:");
     for result in install_results {
         println!("    - {}", result);
     }
